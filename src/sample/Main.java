223e434364ce3831ee4224457581bbe0bea8566b
awardeh <alaahwardeh@hotmail.com>
2019-12-31 00:48:51 -0500

    edited fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pig Latin");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
